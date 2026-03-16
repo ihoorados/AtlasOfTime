132cf9614a05da43ba0ea103040438d53b87e539
Hoorad <Hooradramezani@gmail.com>
2026-03-16 12:07:57 -0700

    feat: add root tab shell for home and settings + previews

--- AtlasOfTime/Presentation/AtlasOfTimeApp.swift
+++ AtlasOfTime/Presentation/AtlasOfTimeApp.swift
@@ -15,7 +15,7 @@ struct AtlasOfTimeApp: App {
 
     var body: some Scene {
         WindowGroup {
-            AtlasScreen(viewModel: viewModel)
+            RootTabScreen(viewModel: viewModel)
         }
     }
 }
