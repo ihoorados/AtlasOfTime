5671f17189824f2ea903354a776ca02b3f3148d2
Hoorad <Hooradramezani@gmail.com>
2026-03-17 15:04:18 -0700

    introduce country-aware year snapshot model

--- AtlasOfTime/Domain/Entities/YearSnapshot.swift
+++ AtlasOfTime/Domain/Entities/YearSnapshot.swift
@@ -2,5 +2,25 @@ import Foundation
 
 struct YearSnapshot: Sendable {
     let year: Int
-    let polygons: [GeoPolygon]
+    let countries: [HistoricalCountry]
+
+    var polygons: [GeoPolygon] {
+        countries.flatMap(\.polygons)
+    }
+
+    init(year: Int, countries: [HistoricalCountry]) {
+        self.year = year
+        self.countries = countries
+    }
+
+    init(year: Int, polygons: [GeoPolygon]) {
+        self.year = year
+        self.countries = [
+            HistoricalCountry(
+                id: "year-\(year)-unattributed",
+                displayName: "Unattributed Borders",
+                polygons: polygons
+            )
+        ]
+    }
 }
