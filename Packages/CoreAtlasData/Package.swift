1b7bba190101925115edbf578a3461627a9231d6
Hoorad <Hooradramezani@gmail.com>
2026-04-02 05:52:26 -0700

    add CoreAtlasData package skeleton

---
+++ Packages/CoreAtlasData/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "CoreAtlasData",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "CoreAtlasData",
+            targets: ["CoreAtlasData"]
+        )
+    ],
+    dependencies: [
+        .package(path: "../CoreAtlasDomain")
+    ],
+    targets: [
+        .target(
+            name: "CoreAtlasData",
+            dependencies: [
+                .product(name: "CoreAtlasDomain", package: "CoreAtlasDomain")
+            ]
+        ),
+        .testTarget(
+            name: "CoreAtlasDataTests",
+            dependencies: ["CoreAtlasData"]
+        )
+    ]
+)
