c234ae75ffe68a21b337dd02f40ffaef35a2d033
Hoorad <Hooradramezani@gmail.com>
2026-04-05 09:24:44 -0700

    add CoreAtlasAppSettings package skeleton

---
+++ Packages/CoreAtlasAppSettings/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "CoreAtlasAppSettings",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "CoreAtlasAppSettings",
+            targets: ["CoreAtlasAppSettings"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "CoreAtlasAppSettings"
+        ),
+        .testTarget(
+            name: "CoreAtlasAppSettingsTests",
+            dependencies: ["CoreAtlasAppSettings"]
+        )
+    ]
+)
