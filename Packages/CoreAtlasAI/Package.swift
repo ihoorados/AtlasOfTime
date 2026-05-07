799bd32608a6fbfbb5879fdd0a8b55a166a6f28e
Hoorad <Hooradramezani@gmail.com>
2026-05-07 19:31:27 +0330

    add mac os platform support

--- Packages/CoreAtlasAI/Package.swift
+++ Packages/CoreAtlasAI/Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "CoreAtlasAI",
     platforms: [
-        .iOS(.v26)
+        .iOS(.v26),
+        .macOS(.v26)
     ],
     products: [
         .library(
--- Packages/CoreAtlasData/Package.swift
+++ Packages/CoreAtlasData/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "CoreAtlasData",
     platforms: [
         .iOS(.v17),
-        .macOS(.v10_15)
+        .macOS(.v14)
     ],
     products: [
         .library(
