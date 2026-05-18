38b1f3570ef9fcfce54511511eab81ab88513ce5
Hoorad <Hooradramezani@gmail.com>
2026-05-19 00:03:12 +0330

    Move map snapshot preparation into view model

--- AtlasOfTime/Features/Home/Presentation/AtlasScreen.swift
+++ AtlasOfTime/Features/Home/Presentation/AtlasScreen.swift
@@ -7,16 +7,12 @@ struct AtlasScreen: View {
     let mapViewFactory: any AtlasMapViewFactory
     let onSelectedCountryTapped: (HistoricalCountrySnapshot) -> Void
     @Environment(\.atlasShowLoadingIndicator) private var showLoadingIndicator
-    private let mapSnapshotMapper = AtlasMapSnapshotMapper()
 
     var body: some View {
         ZStack(alignment: .bottom) {
             mapViewFactory.makeMapView(
                 state: AtlasMapViewState(
-                    snapshot: mapSnapshotMapper.makeSnapshot(
-                        from: viewModel.renderSnapshot,
-                        pointsOfInterest: viewModel.showsPointsOfInterest ? viewModel.pointsOfInterest : []
-                    ),
+                    snapshot: viewModel.mapSnapshot,
                     camera: .world,
                     selection: AtlasMapSelectionState(
                         selectedFeatureID: viewModel.selectedCountryID,
